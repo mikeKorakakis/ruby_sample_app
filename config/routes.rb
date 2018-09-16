d76279a7322656dd4964672b86601e08ef497b34
Mike Korakakis <michael_1988@windowslive.com>
2018-09-15 22:52:49 -0700

    Finish User signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/signup' , to:  "users#new"
+	resources :users
+  get '/signup' , to:  'users#new'
+	post '/signup', to: 'users#create'
 	get '/about', to: 'static_pages#about'
 	get '/contact', to: 'static_pages#contact'
 	get '/help', to: 'static_pages#help'
