cada92258b11f9c60ca388362d0784fc7596a46c
Mike Korakakis <michael_1988@windowslive.com>
2018-09-27 06:46:54 -0700

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,41 +4,42 @@ class UsersController < ApplicationController
     before_action :admin_user, only: :destroy
   
 	def show
-				@user = User.find(params[:id])
+    	@user = User.find(params[:id])
+        redirect_to root_url and return unless @user.activated?
 	end
 
 	def new
-					@user = User.new
-  end
+		@user = User.new
+    end
 
 	def index
-					@users = User.paginate(page: params[:page])
+		@users = User.where(activated: true).paginate(page: params[:page])
 	end
 
 	def create
-					@user = User.new(user_params)
-					if @user.save
-									log_in @user
-									flash[:success] = "Welcome to the Sample App!"
-									redirect_to @user 
-					else
-									render 'new'
-					end
+		@user = User.new(user_params)
+		if @user.save
+           @user.send_activation_email
+            flash[:info] = "Please check you email to activate your account."
+            redirect_to root_url
+		else
+		    render 'new'
+		end
 	end
 
 	def update
-					@user = User.find(params[:id])
-					if @user.update_attributes(user_params)
-									# Handle a successful update
-									flash[:success] = "Profile updated"
-									redirect_to @user
-					else
-									render 'edit'
-					end
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+    # Handle a successful update
+	    	flash[:success] = "Profile updated"
+		redirect_to @user
+		else
+			render 'edit'
+		end
 	end
 
 	def edit
-					@user = User.find(params[:id])
+		@user = User.find(params[:id])
 	end
 
    def destroy
@@ -50,17 +51,17 @@ class UsersController < ApplicationController
 
 	# Confirms a logged-in user.
 	def logged_in_user
-				unless logged_in?
-								store_location
-								flash[:danger] = "Please log in."
-								redirect_to login_url
-				end
+		unless logged_in?
+			store_location
+			flash[:danger] = "Please log in."
+			redirect_to login_url
+		end
 	end
 
 	# Confirms the correct user.
 	def correct_user
-					@user = User.find(params[:id])
-					redirect_to(root_url) unless current_user?(@user)
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
 	end
 
     # Confirms an admin user
@@ -73,6 +74,6 @@ class UsersController < ApplicationController
 
 private
 	def user_params
-					params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
